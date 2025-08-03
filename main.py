dec81f5784717a060303c90eff8c9b924a72d172
kagawayusei <kagawayusei@gmail.com>
2025-08-03 17:22:42 +0900

    学年と趣味追加

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import streamlit as st
 
-st.title("名前記憶アプリ")
+st.title("ユーザー情報入力")
 
-if "name" not in st.session_state:
-    st.session_state.name= ""
+# session_stateの初期化
+if 'user_name' not in st.session_state:
+    st.session_state.user_name = ""
+if 'user_class' not in st.session_state:
+    st.session_state.user_class = ""
+if 'user_hobby' not in st.session_state:
+    st.session_state.user_hobby = ""
 
+# ユーザー名の入力
+name = st.text_input("あなたの名前を入力してください")
+grade = st.selectbox("学年を選択してください",["小５", "小６", "中１", "中２", "中３"])
+hobby = st.multiselect("趣味を選んでください", ["読書", "スポーツ", "音楽", "ゲーム","その他"])
 
-name=st.text_input("あなたの名前を入力してください")
-if st.button("名前を記憶"):
-    st.session_state.name=name
-st.write(f"記憶されている名前: {st.session_state.name}")
+if st.button("情報を保存"):
+    st.session_state.user_name = name
+    st.session_state.user_class = grade
+    st.session_state.user_hobby = hobby
+
+    st.success("情報を保存しました!")
--- pages/page1.py
+++ pages/page1.py
@@ -1,2 +1,23 @@
 import streamlit as st
-st.write("おはようございまーす")
+
+st.title("ユーザー情報表示ページ")
+
+if ('user_name' in st.session_state and st.session_state.user_name):
+    st.success("保存されている情報:")
+    col1,col2 = st.columns(2)
+
+    with col1:
+        st.write("名前:", st.session_state.user_name)
+        st.write("学年:", st.session_state.user_class)
+    with col2:
+        if st.session_state.get('user_hobby'):
+            st.write("**趣味**")
+            for hobby in st.session_state.user_hobby:
+                st.write(f"・ {hobby}")
+        else:
+            st.write("**趣味** 未設定")
+
+    st.balloons()
+else:
+    st.error("ユーザー情報が設定されていません。")
+    st.write("メインページで情報を入力してください。")
